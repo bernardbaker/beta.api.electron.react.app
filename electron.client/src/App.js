8678cfd27f8a2fcd9ba19df21d7cbdade2d8b2bd
bernardbaker <=>
2017-11-27 12:34:10 +0000

    created basic gui with navigation options and dummy data being sent from an api.

--- electron.client/src/App.js
+++ electron.client/src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { 
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
@@ -13,15 +19,75 @@ class App extends Component {
 	
 	render() {
 		return (
-			<div className="App">
-			<h1>Users</h1>
-			{
-				this.state.users.map(user =>
-					<div key={user.id}>{user.username}</div>
-				)
-			}
+			<div>		
+				<div className="App">
+					<h1>Users</h1>
+					{
+						this.state.users.map(user =>
+							<div key={user.id}>{user.username}</div>
+						)
+					}
+				</div>
+				<Navigation/>
 			</div>
 		);
+	}
+}
+
+const Welcome = () => (
+	<div>
+		<h1>Welcome</h1>
+	</div>
+)
+
+const Demo01 = () => (
+	<div>
+		<h1>Demo 01</h1>
+	</div>
+)
+
+const Demo02 = () => (
+	<div>
+		<h1>Demo 02</h1>
+	</div>
+)
+
+const Demo03 = () => (
+	<div>
+		<h1>Demo 03</h1>
+	</div>
+)
+
+const Demo04 = () => (
+	<div>
+		<h1>Demo 04</h1>
+	</div>
+)
+
+class Navigation extends Component {
+	
+	render() {
+		return (
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li><Link to="/">Welcome</Link></li>
+							<li><Link to="/demo01">Demo 01</Link></li>
+							<li><Link to="/demo02">Demo 02</Link></li>
+							<li><Link to="/demo03">Demo 03</Link></li>
+							<li><Link to="/demo04">Demo 04</Link></li>
+						</ul>
+					</nav>
+					<hr/>
+					<Route exact path="/" component={Welcome}/>
+					<Route path="/demo01" component={Demo01}/>
+					<Route path="/demo02" component={Demo02}/>
+					<Route path="/demo03" component={Demo03}/>
+					<Route path="/demo04" component={Demo04}/>					
+				</div>
+			</Router>
+		);	
 	}
 }
 
